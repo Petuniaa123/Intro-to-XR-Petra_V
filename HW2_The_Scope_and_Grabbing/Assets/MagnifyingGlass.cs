545d3501086a048ae54a58ac06fb408f2615df4a
Petuniaa123 <pvaarani21@student.oulu.fi>
2024-02-13 17:19:38 +0200

    HW2_The_Scope_and_Grabbing

---
+++ HW2_The_Scope_and_Grabbing/Assets/MagnifyingGlass.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class MagnifyingGlass : MonoBehaviour
+{
+    public Transform mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.parent.position - mainCamera.position, transform.parent.up);
+    }
+}
---
+++ testi/README.md
@@ -0,0 +1,3 @@
+github: https://github.com/Petuniaa123/Intro-to-XR-Petra_V
+
+I found from Blender website a 3D-dog and I made it to follow the moon and a wooden chair. This website was linked in the instructions.
